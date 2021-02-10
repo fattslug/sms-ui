dbbc06d6f07d73fe386eec940d9a766723b4a087
Sean Powell <ssppowell@gmail.com>
2021-02-09 17:56:58 -0800

    Updating years

--- src/components/sms-date-forms/year-form/YearForm.tsx
+++ src/components/sms-date-forms/year-form/YearForm.tsx
@@ -54,6 +54,8 @@ const YearForm: React.FC<FormProps> = (props) => {
             <option value="2017">2017</option>
             <option value="2018">2018</option>
             <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
           </select>
         </div>
       </form>
